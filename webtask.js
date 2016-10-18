6d0431a8841426920771ff44935804f4c3ad3d68
Adam Chin <chinjazz@gmail.com>
2016-10-18 17:23:53 -0400

    Update for slack post

--- webtask.js
+++ webtask.js
@@ -1,17 +1,23 @@
-module.exports = function(context, callback) {
-    var webHookPayLoad = context.webhook  // contains the webhook payload provided by GitHub
-	var urlQueryWebtaskTokenParams = context.data;
-	
- 	if (!webHookPayLoad)
-	{
-		callback('Invalid payload. Please check your WebHook configuration in GitHub.');
-	}
-	else  callback(null, { some: 'Completed' });
-	
-	function (callback){
-		return callback();
-	}
-}     
+var request = require('request');
+
+return function (context, callback) {
+    if (context.data.slack_token && context.data.slack_channel) {
+        console.log('Posting message to slack for repository ' 
+            + context.webhook.repository.full_name);
+        var url = 'https://slack.com/api/chat.postMessage'
+            + '?token=' + context.data.slack_token
+            + '&channel=' + context.data.slack_channel
+            + '&user=' + (context.data.slack_user || 'WebTask')
+            + '&text=' + encodeURIComponent('Changes in `' + context.webhook.repository.full_name + '`');
+        request({ url: url, method: 'POST' }, function (error, res, body) {
+            callback(error, body);
+        });
+    }
+    else {
+        console.log('Repository ' + context.webhook.repository.full_name + ' changed but slack credentials not supplied.');
+        return callback();
+    }
+}  
 
 
    
