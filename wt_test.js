6141d31113595d46017d47e30170ae910bc5e67c
cjazz <chinjazz@gmail.com>
2016-10-24 07:02:42 -0400

    Update wt_test.js

--- wt_test.js
+++ wt_test.js
@@ -8,7 +8,7 @@ return function (context, callback) {
             + '?token=' + context.data.slack_token
             + '&channel=' + context.data.slack_channel
             + '&user=' + (context.data.slack_user || 'WebTask')
-            + '&text=' + encodeURIComponent('Changes in `' + context.webhook.repository.full_name + '`');
+            + '&text=' + encodeURIComponent('Changes in `' + context.webhook.repository.full_name + 'at: 'context.webhook.repository.html_url + '`');
         request({ url: url, method: 'POST' }, function (error, res, body) {
             callback(error, body);
         });
